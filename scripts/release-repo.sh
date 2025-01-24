29935c45ed6942ae0a1d92134dd82e47cb8000d4
Adam Segal <phazei@gmail.com>
2025-01-24 10:13:55 -0800

    fix: update release script

--- scripts/release-repo.sh
+++ scripts/release-repo.sh
@@ -12,13 +12,12 @@ if ! git show-ref --verify --quiet refs/heads/release; then
 else
     git checkout release
     # Merge current branch changes but prefer our changes in conflicts
-    git merge -X ours $ORIGINAL_BRANCH
+    git merge -X ours $ORIGINAL_BRANCH -m "chore: merge $ORIGINAL_BRANCH changes into release"
 fi
 
 # Temporarily remove lib from .gitignore if it exists
 if grep -q "^lib/$" .gitignore; then
-    sed -i.bak '/^lib\/$/d' .gitignore
-    MODIFIED_GITIGNORE=true
+    sed -i '' '/^lib\/$/d' .gitignore
 fi
 
 # Clean any previous builds
@@ -37,13 +36,8 @@ git add .
 # Commit changes with timestamp
 git commit -m "chore: update release build $(date '+%Y-%m-%d %H:%M:%S')" --allow-empty
 
-# Restore .gitignore if we modified it
-if [ "$MODIFIED_GITIGNORE" = true ]; then
-    mv .gitignore.bak .gitignore
-fi
-
 # Return to original branch
 git checkout $ORIGINAL_BRANCH
 
 echo "Release branch has been updated. You can now use:"
-echo "git+https://github.com/<repo_owner>/react-native-cache-video.git#release"
+echo "git+https://github.com/your-username/react-native-cache-video.git#release"
